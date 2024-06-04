0c73c23bef883c95fe57028d086cd0f1cc73e010
Simon Lawrence <simon@meroxa.io>
2024-06-04 17:07:20 -0400

    Added and updated some logs

--- destination/format/csv.go
+++ destination/format/csv.go
@@ -142,7 +142,7 @@ func MakeCSVBytes(
 	// loop through records and de-dupe before converting to CSV
 	// this is done beforehand, so we can parallelize the CSV formatting
 	latestRecordMap := make(map[string]*recordSummary, len(records))
-	sdk.Logger(ctx).Debug().Msgf("num of records in batch after deduping: %d", len(records))
+	sdk.Logger(ctx).Debug().Msgf("num of records in batch before deduping: %d", len(records))
 
 	for i, r := range records {
 		readAt := r.Metadata["opencdc.readAt"]
@@ -186,6 +186,7 @@ func MakeCSVBytes(
 	dedupedRecords := maps.Values(latestRecordMap)
 	recordChunks := splitRecordChunks(dedupedRecords, numGoroutines)
 
+	sdk.Logger(ctx).Debug().Msgf("num of records in batch before deduping: %d", len(dedupedRecords))
 	sdk.Logger(ctx).Debug().Msgf("processing %d goroutines in %d chunks",
 		numGoroutines, len(recordChunks))
 
